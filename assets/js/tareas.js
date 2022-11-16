58e1a09ad388fb0159032eb1dd3bfb0c677ca980
Cecilia Thielemann <geminis2689@gmail.com>
2022-11-15 23:58:39 -0300

    Terminado al 100%

--- assets/js/tareas.js
+++ assets/js/tareas.js
@@ -1,81 +1,88 @@
 let inputTareas = document.querySelector('#inputTareas');
 let contadorTotal = document.querySelector('#cantTareasTotal');
 let contadorRealizadas = document.querySelector('#cantTareasRealizadas');
-let idCheckbox = document.querySelector('#idCheckbox')
+
 let contadorId = 0;
 let totalTareas = 0;
-let contadorTareasRealizadas = 0;
+let contRealizadas = 0;
 let tareas = [];
 
 let btnAgregar = document.querySelector('#btnAgregar')
-btnAgregar.addEventListener("click", function(){
-//preguntar si inputTareas no esta vacio != ''
-//Para que no agregue espacios vacios
-//Condicion simple
+btnAgregar.addEventListener("click", function () {
+
   const nuevaTarea = inputTareas.value;
   if (nuevaTarea != "") {
-  
-    tareas.push({id: contadorId, nombre: nuevaTarea, completado: false});
+
+    tareas.push({ id: contadorId, nombre: nuevaTarea, completado: false });
     inputTareas.value = "";
     contadorId++;
     totalTareas = tareas.length;
     console.log(nuevaTarea);
     console.log(tareas);
     agregarTarea(tareas)
-} 
-else 
-{
-  alert('Ingresa una tarea')
-}
+  } else {
+    alert('Ingresa una tarea')
+  }
 }
 );
 
 //Agregar Tarea
-let agregarTarea = function(tareasAux) {
-    let idTareasHtml = '';
-    let tareasTotales = '';
-    
-    tareasAux.forEach(tarea => {
-        tareaTemplate = `<tr>
+let agregarTarea = function (tareasAux) {
+  let idTareasHtml = '';
+  let tareasTotales = '';
+ 
+  tareasAux.forEach(tarea => {
+    tareaTemplate = `<tr>
         <td>${tarea.id}</td>
         <td>${tarea.nombre}</td>
-        <td><input type="checkbox" id="idCheckbox" onclick="tareaHecha(${tarea.id})"></td>
+        <td><input type="checkbox" id="idCheckbox" ${tarea.completado && 'checked'} onchange="tareaHecha(${tarea.id})"></td>
         <td><button type="button" id="btnBorrar" class="mx-2 btn btn-danger btn-sm" onclick="borrarTarea(${tarea.id})">Borrar</button></td>
         </tr>`
-       
-        idTareasHtml += tareaTemplate 
-      });
-    
-      let listaFinal = document.querySelector('#tareasId')
-      listaFinal.innerHTML = idTareasHtml
-      tareasTotales = tareas.length
-      contadorTotal.innerHTML = tareasTotales 
-      contadorRealizadas.innerHTML = contadorTareasRealizadas
-  }
- //Cambiar tarea Realizada
+// <td><button type="button" id="boton1" onclick="tareaHecha(${tarea.id})">Terminado</button></td>
+//<td><input type="checkbox" id="idCheckbox" onchange="tareaHecha(${tarea.id})"></td>
+    idTareasHtml += tareaTemplate
+  });
+
+  let listaFinal = document.querySelector('#tareasId')
+  listaFinal.innerHTML = idTareasHtml
+  tareasTotales = tareas.length
+  contadorTotal.innerHTML = tareasTotales
+  // contadorRealizadas.innerHTML = contadorTareasRealizadas
+}
+
+
+//Cambiar tarea Realizada
 
 //Tachar o marcar la tarea ya realizada
-let tareaHecha = function(idTareaHecha){
-  // contadorRealizadas.innerHTML = 0;
+let tareaHecha = function (idTareaHecha) {
+  let tareasRealizadas = '';
 
-let indexTareaHecha = tareas.findIndex((elemento) => elemento.id === idTareaHecha)
-let idCheckbox = document.querySelector('#idCheckbox')
-if (idCheckbox.checked == true){
-  console.log('Checkbox activado')
-  console.log(indexTareaHecha)
-} else {
-        console.log('Checkbox desactivado')
-        console.log(indexTareaHecha)
-    }}
-    
-  
+  let indexTareaHecha = tareas.findIndex((ele) => ele.id === idTareaHecha)
+  tareas[indexTareaHecha].completado = !tareas[indexTareaHecha].completado
 
+
+  console.log(tareas)
+  realizadas()
+  agregarTarea(tareas)
+  // contarRealizadas(tareas)
+ 
+}
+
+let realizadas = function(){
+  const tareasContadas = tareas.filter((tarea) => tarea.completado == true)
+  contRealizadas = tareasContadas.length
+  contadorRealizadas.innerHTML = contRealizadas
+  console.log(tareas)
+  agregarTarea(tareas)
+}
 
 //Borrar una tarea que ya no se quiere
-let borrarTarea = function(idTareaBorrar){
-    let idBorrarTarea = tareas.findIndex((elemento) => elemento.id == idTareaBorrar)
-    if (idBorrarTarea >=0){
-      tareas.splice(idBorrarTarea,1)
-      agregarTarea(tareas)
-    }
-  };
+let borrarTarea = function (idTareaBorrar) {
+  let idBorrarTarea = tareas.findIndex((elemento) => elemento.id == idTareaBorrar)
+  if (idBorrarTarea >= 0) {
+    tareas.splice(idBorrarTarea, 1)
+    console.log(tareas)
+    realizadas()
+    agregarTarea(tareas)
+  }
+};
